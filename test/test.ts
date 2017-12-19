9c9412357e38055b0c6a7c0c7b55793bac5fb094
Hernan Ariel De Luca <hernan@lastrega.com>
2017-12-19 12:56:35 +0100

    added some test for new feature

--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,34 @@
 import * as path from 'path'; 
 import * as fs from 'fs';
-import {Plugin} from './lib/Plugin';
+import {Plugin} from '../src/lib/Plugin';
 
-let decompress = false;
+let decompress = true;
 
-const cwd = path.resolve(__dirname+'/../');
+const cwd = path.resolve(__dirname+'/../test/plugins');
+const compr = path.resolve(__dirname+'/../test/compressed');
+const depl = path.resolve(__dirname+'/../test/toDeploy');
 console.log(cwd);
+console.log(compr);
+console.log(depl);
+
 if(!decompress) {
+const plugin1 = new Plugin('namePlugin','vendor1','1.0.0');
+const plugin2 = new Plugin('namePlugin2','vendor1','1.0.0');
+
+plugin1.cwd = cwd;
+plugin1.dirName = 'vendor1/namePlugin';
+plugin1.compress(compr);
+plugin2.cwd = cwd;
+plugin2.dirName = 'vendor1/namePlugin2';
+plugin2.compress(compr);
+} else {
+    const plugin1 = Plugin.createPluginFromFile('vendor1-namePlugin-1.0.0.tgz');
+    const plugin2 = Plugin.createPluginFromFile('vendor1-namePlugin2-1.0.0.tgz');
+
+    plugin1.decompress(depl,compr);
+    plugin2.decompress(depl,compr);
+}
+/*if(!decompress) {
 
 const plugin = new Plugin('Website','Hardel','1.0.0');
 
@@ -17,4 +39,4 @@ plugin.compress(cwd);
 } else {
     const plugin = Plugin.createPluginFromFile('Hardel-Website-1.0.0.tgz');
     plugin.decompress('folderToUnzip',cwd);
-}
+}*/
